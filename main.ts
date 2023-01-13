ab1cc6c815da021b2df1fa5776434ad72306fe71
anish708 <122548189+anish708@users.noreply.github.com>
2023-01-13 11:26:31 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,18 @@
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Yes)
-    basic.showString("B")
+    basic.showString("Now B")
 })
 input.onButtonPressed(Button.AB, function () {
     basic.showIcon(IconNames.Yes)
+    basic.showIcon(IconNames.Happy)
 })
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Yes)
     basic.showString("A+B")
 })
-input.onGesture(Gesture.Shake, function () {
-    music.playMelody("- - - - - - - - ", 120)
+input.onGesture(Gesture.TiltRight, function () {
+    music.playMelody("C5 B A A G G A B ", 120)
 })
 basic.showString("Hello!")
 basic.showIcon(IconNames.Happy)
+basic.showString("Click A")
