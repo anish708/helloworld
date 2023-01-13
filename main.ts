79634c821e507f974dbeeef07bfb29caf0fd79f7
anish708 <122548189+anish708@users.noreply.github.com>
2023-01-13 11:11:06 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,12 @@
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Yes)
+    basic.showString("B")
 })
 input.onButtonPressed(Button.AB, function () {
-	
+    basic.showIcon(IconNames.Yes)
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showIcon(IconNames.Heart)
+    basic.showIcon(IconNames.Yes)
+    basic.showString("A+B")
 })
 basic.showIcon(IconNames.Happy)
