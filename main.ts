4a0dbc2ec9ec7c3e59a7240f9235bf2048c77fa3
anish708 <122548189+anish708@users.noreply.github.com>
2023-01-13 11:09:11 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Yes)
+})
+input.onButtonPressed(Button.AB, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Heart)
+})
+basic.showIcon(IconNames.Happy)
