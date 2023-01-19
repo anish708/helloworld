8b5c2e02bdb488663bdd8b4cee73c13d3a478e5a
anish708 <122548189+anish708@users.noreply.github.com>
2023-01-19 11:17:36 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,16 +2,48 @@ input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Yes)
     basic.showString("Now B")
 })
+input.onGesture(Gesture.ScreenDown, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        # . # . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        # . # . #
+        `)
+    basic.showIcon(IconNames.Angry)
+})
 input.onButtonPressed(Button.AB, function () {
     basic.showIcon(IconNames.Yes)
     basic.showIcon(IconNames.Happy)
+    basic.showString("Shake")
 })
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Yes)
     basic.showString("A+B")
 })
-input.onGesture(Gesture.TiltRight, function () {
-    music.playMelody("C5 B A A G G A B ", 120)
+input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        # . # . #
+        . # . # .
+        # . # . #
+        . # . # .
+        # . # . #
+        `)
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        . # . # .
+        # . # . #
+        . # . # .
+        `)
+    basic.showString("Don't flip me")
 })
 basic.showString("Hello!")
 basic.showIcon(IconNames.Happy)
