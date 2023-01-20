81884ccea9d629b82e0b73fa0eb0c1cdac30d94a
anish708 <122548189+anish708@users.noreply.github.com>
2023-01-20 11:05:20 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -32,6 +32,10 @@ input.onGesture(Gesture.Shake, function () {
         `)
     basic.showString("Don't flip me")
 })
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    music.playMelody("B B A G E B F C5 ", 180)
+    music.playMelody("A F B G C5 A B G ", 180)
+})
 basic.showString("Hello!")
 basic.showIcon(IconNames.Happy)
 basic.showString("Click A")
