2be4a15efede721d22baf09ef2f9de8077704120
anish708 <122548189+anish708@users.noreply.github.com>
2023-01-20 11:10:21 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
     music.playMelody("B B A G E B F C5 ", 180)
-    music.playMelody("C5 G B F C5 A B G ", 180)
+    music.playMelody("G C5 B F C5 A B G ", 180)
 })
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Yes)
